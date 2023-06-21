a215cf1bc51fac29e366227f791c8c388ffc6ef4
Deniz Cakmak <d.cakmak@emakina.com.tr>
2023-06-21 19:49:50 +0300

    Add PrintNumbersInRange method to PrimeComposite class.

---
+++ KatasTests/PrimeCompositeTests.cs
@@ -0,0 +1,26 @@
+﻿using Katas;
+
+namespace KatasTests
+{
+    /// <summary>
+    /// PrimeComposite tests.
+    /// </summary>
+    public class PrimeCompositeTests
+    {
+        private PrimeComposite primeComposite;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.primeComposite = new PrimeComposite();
+        }
+
+        [TestCase(1, 100)]
+        public void PrintNumbersInRangeShouldPrintNumbersFromStartToEnd(int start, int end)
+        {
+            var expected = string.Join(Environment.NewLine, Enumerable.Range(1, 100));
+            var actual = this.primeComposite.PrintNumbersInRange(start, end);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
