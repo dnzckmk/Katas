30490caaa97c78e094a7a4d92142069b57e63e7f
Deniz Cakmak <d.cakmak@emakina.com.tr>
2023-06-21 15:19:09 +0300

    Add OddEvenPrime logic into PrintNumbersInRange method.

--- Katas/OddEven.cs
+++ Katas/OddEven.cs
@@ -1,21 +1,4 @@
 ﻿/*
-## OddEven Kata
-#### Goal
-Write a program that prints numbers within specified range lets say 1 to 100.
-If number is odd print 'Odd' instead of the number.
-If number is even print 'Even' instead of number.
-Else print number [hint - if number is Prime].
-
-#### Steps
-Lets divide into following steps:
-* Prints numbers from 1 to 100
-* Print "Even" instead of number, if the number is even, means divisible by 2
-* Print "Odd" instead of number, if the number is odd, means not divisible by 2 but not divisible by itself too [hint - it should not be Prime]
-* Print number, if it does not meet above two conditions, means if number is Prime
-* Make method to accept any number of range [currently  we have 1 to 100]
-* Create a new method to check Odd/Even/Prime of a single supplied method
-
-**************
 ### The OddEven Kata
 Write a program that prints numbers within specified range lets say 1 to 100.
 If number is odd print 'Odd' instead of the number.
@@ -35,18 +18,71 @@ using System.Text;
 
 namespace Katas
 {
+    /// <summary>
+    /// OddEven class.
+    /// </summary>
     public class OddEven
     {
+        /// <summary>
+        /// Print numbers between given range. Uses EvenOddPrime logic.
+        /// </summary>
+        /// <param name="start">Start of the range.</param>
+        /// <param name="end">End of the range.</param>
+        /// <returns>Returns values as a string.</returns>
         public string PrintNumbersInRange(int start, int end)
         {
             var result = new StringBuilder();
 
             for (int number = start; number <= end; number++)
             {
-                result.AppendLine(number.ToString());
+                result.AppendLine(this.EvenOddPrime(number));
             }
 
             return result.ToString().TrimEnd();
+        }
+
+        /// <summary>
+        /// Check if the number is even, odd or prime. 
+        /// </summary>
+        /// <param name="number">Integer number to check.</param>
+        /// <returns>Returns "Even" for even number, "Odd" for odd values and number as a string if the value is a prime number.</returns>
+        public string EvenOddPrime(int number)
+        {
+            if (number % 2 == 0)
+            {
+                return "Even";
+            }
+            else if (!IsPrime(number))
+            {
+                return "Odd";
+            }
+            else
+            {
+                return number.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Check if the number is prime.
+        /// </summary>
+        /// <param name="number">Integer number to check.</param>
+        /// <returns>Returns true if the number is prime else returns false.</returns>
+        private bool IsPrime(int number)
+        {
+            if (number < 2)
+            {
+                return false;
+            }
+
+            for (int divisor = 2; divisor <= Math.Sqrt(number); divisor++)
+            {
+                if (number % divisor == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
--- KatasTests/OddEvenTests.cs
+++ KatasTests/OddEvenTests.cs
@@ -2,6 +2,9 @@
 
 namespace KatasTests
 {
+    /// <summary>
+    /// OddEven test class.
+    /// </summary>
     public class OddEvenTests
     {
         private OddEven oddEven;
@@ -15,11 +18,37 @@ namespace KatasTests
         [TestCase(1, 100)]
         public void PrintNumbersInRangeShouldReturnNumbersFrom1To100(int start, int end)
         {
-            var expected = string.Join(Environment.NewLine, Enumerable.Range(1, 100));
+            var expected = "Odd\r\nEven\r\n3\r\nEven\r\n5\r\nEven\r\n7\r\nEven\r\nOdd\r\nEven\r\n11\r\nEven\r\n13\r\nEven\r\nOdd\r\nEven\r\n17\r\nEven\r\n19\r\nEven\r\nOdd\r\nEven\r\n23\r\nEven\r\nOdd\r\nEven\r\nOdd\r\nEven\r\n29\r\nEven\r\n31\r\nEven\r\nOdd\r\nEven\r\nOdd\r\nEven\r\n37\r\nEven\r\nOdd\r\nEven\r\n41\r\nEven\r\n43\r\nEven\r\nOdd\r\nEven\r\n47\r\nEven\r\nOdd\r\nEven\r\nOdd\r\nEven\r\n53\r\nEven\r\nOdd\r\nEven\r\nOdd\r\nEven\r\n59\r\nEven\r\n61\r\nEven\r\nOdd\r\nEven\r\nOdd\r\nEven\r\n67\r\nEven\r\nOdd\r\nEven\r\n71\r\nEven\r\n73\r\nEven\r\nOdd\r\nEven\r\nOdd\r\nEven\r\n79\r\nEven\r\nOdd\r\nEven\r\n83\r\nEven\r\nOdd\r\nEven\r\nOdd\r\nEven\r\n89\r\nEven\r\nOdd\r\nEven\r\nOdd\r\nEven\r\nOdd\r\nEven\r\n97\r\nEven\r\nOdd\r\nEven";
 
-            string actual = this.oddEven.PrintNumbersInRange(1, 100);
-
+            string actual = this.oddEven.PrintNumbersInRange(start, end);
             Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [TestCase(2)]
+        [TestCase(40)]
+        [TestCase(98)]
+        public void EvenOddPrimeShouldReturnEvenForMultipleof2(int number)
+        {
+            var result = this.oddEven.EvenOddPrime(number);
+            Assert.That(result, Is.EqualTo("Even"));
+        }
+
+        [TestCase(1)]
+        [TestCase(25)]
+        [TestCase(99)]
+        public void EvenOddPrimeShouldReturnOddForNotEvenOrPrimeNumbers(int number)
+        {
+            var result = this.oddEven.EvenOddPrime(number);
+            Assert.That(result, Is.EqualTo("Odd"));
+        }
+
+        [TestCase(3, "3")]
+        [TestCase(11, "11")]
+        [TestCase(97, "97")]
+        public void EvenOddPrimeShouldReturnNumberAsStringForPrimeNumbers(int number, string expected)
+        {
+            var result = this.oddEven.EvenOddPrime(number);
+            Assert.That(result, Is.EqualTo(expected));
         }
     }
 }
