9b86be71fbbd5c9eb3a6bca863ab1c32df0735c1
Deniz Cakmak <d.cakmak@emakina.com.tr>
2023-06-21 14:33:38 +0300

    Add OddEven class with PrintNumbersInRange method

---
+++ KatasTests/OddEvenTests.cs
@@ -0,0 +1,25 @@
+﻿using Katas;
+
+namespace KatasTests
+{
+    public class OddEvenTests
+    {
+        private OddEven oddEven;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.oddEven = new OddEven();
+        }
+
+        [TestCase(1, 100)]
+        public void PrintNumbersInRangeShouldReturnNumbersFrom1To100(int start, int end)
+        {
+            var expected = string.Join(Environment.NewLine, Enumerable.Range(1, 100));
+
+            string actual = this.oddEven.PrintNumbersInRange(1, 100);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
